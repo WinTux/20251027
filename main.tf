e16caee34ab5454801bbfb0d86ed323003fa8ca8
rusok <rusok.software@gmail.com>
2025-11-04 21:22:14 -0400

    Preparando Ansible

--- main.tf
+++ main.tf
@@ -11,14 +11,15 @@ provider "aws" {
 }
 
 resource "aws_instance" "mi_servidor" {
-  for_each = var.nombres_servicios
+  #for_each = var.nombres_servicios
+  count = local.nombre_workspace == "produccion" ? 2 : 1
   ami           = "ami-0023593d16b53b3e9"
   instance_type = "t3.micro"
   subnet_id = module.vpc.public_subnets[0]
   vpc_security_group_ids = [module.security-group.security_group_id]
   associate_public_ip_address = true
   tags = {
-    Nombre    = "ServidorTerraform-${each.key}"
+    Nombre    = format("%s-%s",local.nombre_workspace,count.index)  #"ServidorTerraform-${each.key}"
     Owner     = ""
     Instancia = "EC2: Elastic Compute Cloud"
     AlgunTag = local.tag_name
@@ -27,6 +28,11 @@ resource "aws_instance" "mi_servidor" {
 
 locals {
   tag_name = "un-tag"
+  nombre_workspace = terraform.workspace
+  ruta_private_key = "home/rusok/Documentos/DevOps/ejemploTerraform/clasesdevops.pem"
+  nombre_key = "clasesdevops"
+  usuario_ssh = "ubuntu"
+  # continuar en resources
 }
 
 #data "aws_subnet" "default" {
